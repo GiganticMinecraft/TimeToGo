26a873319816d7b9e39c191689f83df0c4ea09bc
Lucky3028 <Lucky3028@users.noreply.github.com>
2022-09-03 13:57:31 +0900

    feat: do DI

--- src/main/scala/click/seichi/timetogo/presenter/TimeToGo.scala
+++ src/main/scala/click/seichi/timetogo/presenter/TimeToGo.scala
@@ -1,10 +1,30 @@
 package click.seichi.timetogo.presenter
 
+import click.seichi.timetogo.infra.ModeTimeRepositoryImpl
+import click.seichi.timetogo.model.ModeTimeRepository
+import click.seichi.timetogo.usecase.ModeTimeUseCase
+import click.seichi.timetogo.usecase.`trait`.Clock
 import org.bukkit.Bukkit
 import org.bukkit.plugin.java.JavaPlugin
 
+import java.time.LocalTime
+
 class TimeToGo extends JavaPlugin {
+  TimeToGo.instance = this
+
   override def onEnable(): Unit = {
     Bukkit.getServer.getLogger.info("TimeToGo is Enabled!")
+  }
+}
+
+object TimeToGo {
+  private var instance: TimeToGo = _
+
+  lazy val useCase: ModeTimeUseCase = new ModeTimeUseCase {
+    val clock: Clock = new Clock {
+      def now: LocalTime = LocalTime.now
+    }
+
+    val repository: ModeTimeRepository = ModeTimeRepositoryImpl(instance)
   }
 }

cd0748057aed12734f5ca14f7295aac3a6987a70
Lucky3028 <Lucky3028@users.noreply.github.com>
2022-09-03 16:56:41 +0900

    fix: fix cast

--- src/main/scala/click/seichi/timetogo/infra/ModeTimeRepositoryImpl.scala
+++ src/main/scala/click/seichi/timetogo/infra/ModeTimeRepositoryImpl.scala
@@ -1,12 +1,21 @@
 package click.seichi.timetogo.infra
 
-import click.seichi.timetogo.model.{ModeTime, ModeTimeRepository}
+import click.seichi.timetogo.model.{GameMode, ModeTime, ModeTimeRepository}
 import org.bukkit.plugin.java.JavaPlugin
 
+import java.time.LocalTime
+import java.util
 import scala.jdk.CollectionConverters._
+import scala.util.Try
 
 case class ModeTimeRepositoryImpl(instance: JavaPlugin) extends ModeTimeRepository {
   private def config = instance.getConfig
 
-  override def list(): List[ModeTime] = config.getList("mode-time").asScala.toList.map(_.asInstanceOf[ModeTime])
+  override def list: List[ModeTime] = {
+    for {
+      map <- config.getList("mode-time").asScala.toList.asInstanceOf[List[util.LinkedHashMap[String, String]]]
+      gameMode <- GameMode.fromString(map.get("game-mode"))
+      time <- Try(LocalTime.parse(map.get("time"))).toOption
+    } yield ModeTime(gameMode, time)
+  }
 }

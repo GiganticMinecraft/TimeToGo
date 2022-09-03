90942bab4232e9b9ab982c6930e92326983b1593
Lucky3028 <Lucky3028@users.noreply.github.com>
2022-09-03 15:01:16 +0900

    feat: run the task in each minute

--- src/main/scala/click/seichi/timetogo/presenter/TimeToGo.scala
+++ src/main/scala/click/seichi/timetogo/presenter/TimeToGo.scala
@@ -2,18 +2,33 @@ package click.seichi.timetogo.presenter
 
 import click.seichi.timetogo.infra.ModeTimeRepositoryImpl
 import click.seichi.timetogo.model.ModeTimeRepository
+import click.seichi.timetogo.presenter.listener.PlayerLoginListener
+import click.seichi.timetogo.presenter.task.SetPlayerGameMode
 import click.seichi.timetogo.usecase.ModeTimeUseCase
 import click.seichi.timetogo.usecase.`trait`.Clock
 import org.bukkit.Bukkit
 import org.bukkit.plugin.java.JavaPlugin
+import org.bukkit.scheduler.BukkitTask
 
 import java.time.LocalTime
 
 class TimeToGo extends JavaPlugin {
+  private var task: BukkitTask = _
+
   TimeToGo.instance = this
 
   override def onEnable(): Unit = {
+    Bukkit.getPluginManager.registerEvents(PlayerLoginListener, this)
+
+    this.task = SetPlayerGameMode.runTaskTimer(this, 0L, 20 * 60L)
+
     Bukkit.getServer.getLogger.info("TimeToGo is Enabled!")
+  }
+
+  override def onDisable(): Unit = {
+    this.task.cancel()
+
+    Bukkit.getServer.getLogger.info("TimeToGo is Disabled!")
   }
 }
 
